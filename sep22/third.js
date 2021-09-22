e4955c5a0229fe5e4b76964b43fc95495c67bb13
Mausam <mauasam15171098@gmail.com>
2021-09-22 19:18:34 +0530

    initial commit

---
+++ sep22/third.js
@@ -0,0 +1,24 @@
+function runProgram(input) {
+    input = input.split("\n");
+  
+    var size = Number(input[0]);
+    var array = input[1].split(" ").map(Number);
+  
+    
+    
+  }
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
